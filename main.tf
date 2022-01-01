9235110ee1fb33a72fe5aca6fb5b54dd67b77528
mutao-net <sasaki.h1026@gmail.com>
2022-01-01 15:11:51 +0900

    add s3 module

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 module "web_server" {
-    source = "./httpd"
-    instance_type = "t2.micro"
+  source        = "./httpd"
+  instance_type = "t2.micro"
+}
+
+module "s3_storage" {
+  source = "./s3"
 }
 
 output "public_dns" {
-    value = module.web_server.public_dns
+  value = module.web_server.public_dns
 }
-
---
+++ s3/main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "private" {
+  bucket = "mutao-tf-bucket"
+  versioning {
+    enabled = false
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
